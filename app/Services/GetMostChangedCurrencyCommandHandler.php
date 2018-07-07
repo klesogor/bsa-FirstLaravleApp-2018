8e6befa1068b37b284b22bee10936c0b3f6f7e4d
Лесогорский Кирилл <lesogor.kirill@gmail.com>
2018-07-07 22:35:24 +0300

    Now unstable currencies compared in absolute values

--- app/Services/GetMostChangedCurrencyCommandHandler.php
+++ app/Services/GetMostChangedCurrencyCommandHandler.php
@@ -11,7 +11,7 @@ class GetMostChangedCurrencyCommandHandler
     {
         return $this->getWrappedCoins()
             ->sortByDesc(function($item) {
-                return $item->getDailyChangePercent();
+                return abs($item->getDailyChangePercent());
             })
             ->first();
     }
